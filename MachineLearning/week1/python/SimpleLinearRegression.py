57671ab39b34d1d544e027e7033234a6e77ca181
Vishnu R <vishnurapps@gmail.com>
2018-06-26 22:30:05 +0530

    Plotting N iterations

--- MachineLearning/week1/python/SimpleLinearRegression.py
+++ MachineLearning/week1/python/SimpleLinearRegression.py
@@ -37,3 +37,43 @@ J_grid = cost_func(theta1_grid[:,np.newaxis])
 
 # The cost function as a function of its single parameter, theta1.
 ax[1].plot(theta1_grid, J_grid, 'k')
+
+# Take N steps with learning rate alpha down the steepest gradient,
+# starting at theta1 = 0.
+N = 5
+alpha = 1
+theta1 = [0]
+J = [cost_func(theta1[0])[0]]
+for j in range(N-1):
+    last_theta1 = theta1[-1]
+    this_theta1 = last_theta1 - alpha / m * np.sum(
+                                    (hypothesis(x, last_theta1) - y) * x)
+    theta1.append(this_theta1)
+    J.append(cost_func(this_theta1))
+    
+# Annotate the cost function plot with coloured points indicating the
+# parameters chosen and red arrows indicating the steps down the gradient.
+# Also plot the fit function on the LHS data plot in a matching colour.
+colors = ['b', 'g', 'm', 'c', 'orange']
+ax[0].plot(x, hypothesis(x, theta1[0]), color=colors[0], lw=2,
+           label=r'$\theta_1 = {:.3f}$'.format(theta1[0]))
+for j in range(1,N):
+    ax[1].annotate('', xy=(theta1[j], J[j]), xytext=(theta1[j-1], J[j-1]),
+                   arrowprops={'arrowstyle': '->', 'color': 'r', 'lw': 1},
+                   va='center', ha='center')
+    ax[0].plot(x, hypothesis(x, theta1[j]), color=colors[j], lw=2,
+               label=r'$\theta_1 = {:.3f}$'.format(theta1[j]))
+
+# Labels, titles and a legend.
+ax[1].scatter(theta1, J, c=colors, s=40, lw=0)
+ax[1].set_xlim(-0.2,1)
+ax[1].set_xlabel(r'$\theta_1$')
+ax[1].set_ylabel(r'$J(\theta_1)$')
+ax[1].set_title('Cost function')
+ax[0].set_xlabel(r'$x$')
+ax[0].set_ylabel(r'$y$')
+ax[0].set_title('Data and fit')
+ax[0].legend(loc='upper left', fontsize='small')
+
+plt.tight_layout()
+plt.show()
