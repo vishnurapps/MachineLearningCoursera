885c883ab50948c47c4ce4d1a87b6f324b45d44e
Vishnu R <vishnurapps@gmail.com>
2018-06-26 20:46:59 +0530

    functions to calculate value of hyposthesis and cost

--- MachineLearning/week1/python/SimpleLinearRegression.py
+++ MachineLearning/week1/python/SimpleLinearRegression.py
@@ -14,3 +14,14 @@ m = 20
 theta1_true = 0.5
 x = np.linspace(-1,1,m)
 y = theta1_true * x
+
+#functions to calculate value of hyposthesis and cost
+
+def cost_func(theta1):
+    """The cost function, J(theta1) describing the goodness of fit."""
+    theta1 = np.atleast_2d(np.asarray(theta1))
+    return np.average((y-hypothesis(x, theta1))**2, axis=1)/2
+
+def hypothesis(x, theta1):
+    """Our "hypothesis function", a straight line through the origin."""
+    return theta1*x
