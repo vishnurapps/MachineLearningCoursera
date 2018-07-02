a097109f9cffc2859c43abac5ef95f6e2e999ac0
Vishnu R <vishnurapps@gmail.com>
2018-07-02 23:56:40 +0530

    Reading the input file

---
+++ MachineLearning/week1/octave/read_input.m
@@ -0,0 +1,23 @@
+
+#To load dataset to octave use the load methods
+load('sampleData.dat')
+
+#To print the variables in memory we can use who command
+who
+
+#To print the variables in memory we can use who command
+whos
+
+#TO get the size of a variable, we can use size command
+size('sampleData.dat')
+
+#To clear a variable from memory, we can use the clear command
+clear sampleData
+whos
+
+#To save a variable to a file
+save hello.mat v
+
+#To delete all the variables, we can simply use clear
+clear
+load('hello.mat')
